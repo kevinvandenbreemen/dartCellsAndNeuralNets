ead1c024c59236144a4f52aa8de4a95b8469ee0d
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-07-21 15:32:43 -0400

    Add test that randomly adds/deletes cells as well as computing inputs

--- test/monkey_test.dart
+++ test/monkey_test.dart
@@ -8,7 +8,7 @@ import 'package:test/test.dart';
 
 main() {
   
-  const int iterations = 20;
+  const int iterations = 500;
   Random random = Random();
   List<List<double>> rawWeightMatrix;
   void addOutgoingCellOnMatrix(Tissue originTissue) {
@@ -29,7 +29,21 @@ main() {
     }
     rawWeightMatrix.add(ret);
   }
+
+  void deleteOutgoingCellOnMatrix(int index) {
+    rawWeightMatrix.forEach((row) => row.removeAt(index));
+  }
+
+  void deleteIncomingCellOnMatrix(int indexToDelete) {
+    rawWeightMatrix.removeAt(indexToDelete);
+  }
+
   void addConnection(int from, int to, double strength) {
+    if(rawWeightMatrix.isEmpty) {
+      rawWeightMatrix.add(List<double>());
+      rawWeightMatrix[0].add(strength);
+      return;
+    }
     rawWeightMatrix[to][from] = strength;
   }
 
@@ -65,10 +79,56 @@ main() {
         expect(expectedOutput, resultOfInputs, reason: "Weight matrix: $rawWeightMatrix, Inputs: $values");
 
       }
+    });
 
-      print("Final weight matrix:\n$rawWeightMatrix");
+    test('Adding and deleting cells', () {
+      for(var i=0; i<iterations; i++){
+
+        if(t1.cellCount > 0 && t2.cellCount > 0 && random.nextBool()) {
+          if(random.nextBool()){
+            int indexToDelete = random.nextInt(t1.cellCount);
+            if(indexToDelete == t1.cellCount) {
+              indexToDelete --;
+            }
+            t1.deleteCell(indexToDelete);
+            deleteOutgoingCellOnMatrix(indexToDelete);
+          } else {
+            int indexToDelete = random.nextInt(t2.cellCount);
+            if(indexToDelete == t2.cellCount) {
+              indexToDelete --;
+            }
+            t2.deleteCell(indexToDelete);
+            deleteIncomingCellOnMatrix(indexToDelete);
+          }
+        }
+
+        t1.add(STEM);
+        t2.add(STEM);
+        addOutgoingCellOnMatrix(t1);
+        addIncomingCellOnMatrix(t2);
+
+        int from = random.nextInt(t1.cellCount);
+        int to = random.nextInt(t2.cellCount);
+        double strength = random.nextDouble();
+
+        t1.connectToTissue(t2, from: from, to: to, strength: strength);
+        addConnection(from, to, strength);
+
+        List<double> values = List<double>.generate(t1.cellCount, (k)=>0.5);
+        try {
+          
+          //List<double> expectedOutput = (Matrix(rawWeightMatrix) * Vector.column(values)).columnVector(0).toList();
+          List<double> resultOfInputs = t1.connectedTissues()[0].computeOutputFor(values);
+
+          //expect(expectedOutput, resultOfInputs, reason: "Weight matrix: $rawWeightMatrix, Inputs: $values");
+        } catch(ex){
+          fail("Unexpected ${ex.toString()}:  Raw matrix = $rawWeightMatrix, input:  $values");
+        }
+
+      }
     });
 
   });
 
 }
+
