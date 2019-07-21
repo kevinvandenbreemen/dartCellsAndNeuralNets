f9942e096021319fe1911c6f7d35e8392b6ecf68
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-07-21 15:55:35 -0400

    Fix naming for publish

---
+++ lib/neural_net_experiments.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library neural_net_experiments;
+
+export 'src/neural_net_experiments_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
